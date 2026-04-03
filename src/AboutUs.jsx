f4022b961fa106e1cddb562c0f73e198a247dd74
jfp-cc <justiceforpookiecoin@gmail.com>
2026-04-03 12:50:52 +0530

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-description">Welcome to Paradise Nursery!</p>
+      <p className="about-us-content">
+        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide 
+        a wide range of high-quality plants that not only enhance the beauty of your surroundings but also 
+        contribute to a healthier and more sustainable lifestyle. From air-purifying plants to aromatic 
+        medicinal herbs, we have something for every plant lover.
+      </p>
+      <p className="about-us-content">
+        Join us in our journey to make the world a greener place, one plant at a time.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
